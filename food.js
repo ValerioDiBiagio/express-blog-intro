882bbda3d08f59cedf112c042e571be9dd935da1
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-03-26 16:33:44 +0100

    feat: create array

---
+++ food.js
@@ -0,0 +1,32 @@
+const foods = [
+    {
+        titolo: 'Ciambellone',
+        contenuto: 'Dolce',
+        immagine: "imgs/food/ciambellone.jpeg",
+        tags: ['#dolce', '#ciambellone'],
+    },
+    {
+        titolo: 'Cracker di barbabietola',
+        contenuto: 'Cracker',
+        immagine: "imgs/food/cracker_barbabietola.jpeg",
+        tags: ['#cracker', '#barbabietola'],
+    },
+    {
+        titolo: 'Pane fritto dolce',
+        contenuto: 'Pane',
+        immagine: "imgs/food/pane_fritto_dolce.jpeg",
+        tags: ['#pane', '#fritto'],
+    },
+    {
+        titolo: 'Pasta con barbabietola',
+        contenuto: 'Pasta',
+        immagine: "imgs/food/pasta_barbabietola.jpeg",
+        tags: ['#pasta', '#barbabietola'],
+    },
+    {
+        titolo: 'Torta paesana',
+        contenuto: 'Torta',
+        immagine: "imgs/food/torta_paesana.jpeg",
+        tags: ['#torta', '#paesana'],
+    }
+]
