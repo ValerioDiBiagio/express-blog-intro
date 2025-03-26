a94d954813753221a1a10708ad276461fedc8820
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-03-26 15:51:05 +0100

    feat: import express and create route

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog');
+})
+
+app.listen(port, () => {
+    console.log('Sono un server attivo sulla porta ' + port);
+})
